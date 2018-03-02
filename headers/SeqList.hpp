5b74786216aad8d064e284fd558242ec795f1f0a
tangwen <inryygy007@gmail.com>
2018-03-02 12:59:54 +0800

    完善多项式应用

--- headers/SeqList.hpp
+++ headers/SeqList.hpp
@@ -93,12 +93,200 @@ Status destroyPolyn(PolyList &P) {
 	return OK;
 }
 
-Status addPolyn(PolyList &pa, PolyList &pb)
+Status unattachOneItem(p_node p)
 {
-
+	p->prev->next = p->next;
+	p->next->prev = p->prev;
+	return OK;
 }
 
+Status attachOneItem(p_node p, p_node pos)
+{
+	p->next = pos->next;
+	pos->next = p;
 
+	p->prev = p->next->prev;
+	p->next->prev = p;
+
+	return OK;
+}
+
+Status delOneItem(p_node &pos)
+{
+	unattachOneItem(pos);
+	free(pos);
+	pos = NULL;
+	return OK;
+}
+
+Status locateExp(PolyList L, int exp,p_node &locate_p)
+{
+	p_node p = L.head;
+	while (p->next != L.head)
+	{
+		p = p->next;
+		if (p->exp == exp)
+		{
+			locate_p = p;
+			break;
+		}
+	}
+	return OK;
+}
+
+Status clonePoly(PolyList L, PolyList &L_dist)
+{
+	L_dist.head = (PolyNode*)malloc(sizeof(PolyNode));
+	L_dist.head->next = L_dist.head;
+	L_dist.head->prev = L_dist.head;
+
+	p_node p = L.head;
+
+	p_node tp = L_dist.head;
+
+	while (p->next != L.head)
+	{
+		p = p->next;
+		p_node pc = (PolyNode*)malloc(sizeof(PolyNode));
+		
+		pc->coe = p->coe;
+		pc->exp = p->exp;
+
+		tp->next = pc;
+		pc->prev = tp;
+
+		pc->next = L_dist.head;
+		L_dist.head->prev = pc;
+
+		tp = pc;
+	}
+
+	return OK;
+}
+
+Status multiOneItem(PolyList &L, p_node pItem)
+{
+	p_node p = L.head;
+	while (p->next != L.head)
+	{
+		p = p->next;
+		p->coe *= pItem->coe;
+		p->exp += pItem->exp;
+	}
+	return OK;
+}
+
+Status locateExpProximity(PolyList L, int exp, p_node &proPos)
+{
+	p_node p = L.head;
+	while (p->next != L.head)
+	{
+		p = p->next;
+		if (p->exp > exp)
+		{
+			proPos = p->prev;
+			break;
+		}
+
+		if (p->next == L.head)
+		{
+			proPos = p;
+		}
+	}
+
+	return OK;
+}
+
+Status addPolyn(PolyList &pa, PolyList &pbOri)
+{
+	PolyList pb;
+	clonePoly(pbOri,pb);
+
+	p_node paH = pa.head;
+
+	p_node pbH = pb.head;
+
+	while (pbH->next != pbH)
+	{
+		p_node pb1 = pbH->next;
+		unattachOneItem(pb1);
+
+		p_node findp = NULL;
+		locateExp(pa, pb1->exp, findp);
+		if (findp) {
+			findp->coe += pb1->coe;
+			if (findp->coe == 0) {
+				delOneItem(findp);
+			}
+
+			delOneItem(pb1);
+		}
+		else {
+			locateExpProximity(pa, pb1->exp, findp);
+			attachOneItem(pb1, findp);
+		}
+	}
+	destroyPolyn(pb);
+
+	return OK;
+}
+
+Status subPolyn(PolyList &pa, PolyList &pb)
+{
+	PolyList temp;
+	p_node p = (PolyNode*)malloc(sizeof( PolyNode));
+	p->coe = -1;
+	p->exp = 0;
+	p->prev = p->next = NULL;
+
+	clonePoly(pb, temp);
+	multiOneItem(temp, p);
+
+	addPolyn(pa, temp);
+
+	free(p);
+	p = NULL;
+
+	destroyPolyn(temp);
+
+	return OK;
+}
+
+Status mulPolyn(PolyList &pa, PolyList &pbOri)
+{
+	PolyList pb;
+	clonePoly(pbOri, pb);
+
+	p_node paH = pa.head;
+	p_node pbH = pb.head;
+
+	PolyList oritemp;
+	clonePoly(pa, oritemp);
+
+	bool isFirst = false;
+	while (pbH->next != pbH) {
+		p_node pb1 = pbH->next;
+		unattachOneItem(pb1);
+
+		if (!isFirst)
+		{
+			isFirst = true;
+			multiOneItem(pa, pb1);
+		}
+		else
+		{
+			PolyList temp;
+			clonePoly(oritemp, temp);
+			multiOneItem(temp, pb1);
+			addPolyn(pa, temp);
+			destroyPolyn(temp);
+		}
+	}
+	destroyPolyn(oritemp);
+	destroyPolyn(pb);
+
+	return OK;
+}
 
 
 #endif
--- tw_tests/SeqListTest.cpp
+++ tw_tests/SeqListTest.cpp
@@ -9,8 +9,36 @@ int main()
 	initPoly(p1, f1, e1, sizeof(e1) / sizeof(int));
 	printPolyn(p1);
 
-	destroyPolyn(p1);
+	PolyList p2;
+	float f2[] = { 2.0f,4.0f,-9.1f,5.0f,66.0f };
+	int e2[] = { 0,1,8,17,19 };
 
+	initPoly(p2, f2, e2, sizeof(e2) / sizeof(int));
+	printPolyn(p2);
+
+	PolyList p11;
+	clonePoly(p1, p11);
+	std::cout << "p1 + p2" << std::endl;
+	addPolyn(p11, p2);
+	printPolyn(p11);
+	destroyPolyn(p11);
+
+	PolyList p12;
+	clonePoly(p1, p12);
+	std::cout << "p1 * p2" << std::endl;
+	mulPolyn(p12, p2);
+	printPolyn(p12);
+	destroyPolyn(p12);
+
+	PolyList p13;
+	clonePoly(p1, p13);
+	std::cout << "p1 - p2" << std::endl;
+	subPolyn(p13, p2);
+	printPolyn(p13);
+	destroyPolyn(p13);
+
+	destroyPolyn(p1);
+	destroyPolyn(p2);
 
 	system("pause");
 	return 0;
